8aeb8602c04e41048d4b684b93fedfb4830f1507
saad naikwade <naikwadesaad@gmail.com>
2025-10-09 11:39:56 +0530

    updated amstrong...

--- leetcode/basics/amstrong_number.py
+++ leetcode/basics/amstrong_number.py
@@ -1,4 +1,4 @@
-# amstrong number - if a number is equal to cube of individual digit of sum then it is amstrong 
+# amstrong number - if a number is equal to lenof power of individual digit of sum then it is amstrong 
 # 153 = 1^3+5^3+3^3
 #     = 1 + 125 + 27
 #     = 153
@@ -6,8 +6,9 @@
 n=int(input())
 m=n
 sum=0
+pow=len(str(n))
 while m!=0:
-    sum=sum+((m%10)**3)
+    sum=sum+((m%10)**pow)
     m=m//10
 if n==sum:
     print("amstrong number")
