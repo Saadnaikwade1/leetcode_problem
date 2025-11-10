086815a465e8eb4ad0d54f6f70cccc33b6502b5e
Saad Naikwade <134310173+Saadnaikwade1@users.noreply.github.com>
2025-11-10 15:48:36 +0530

    Time: 249 ms (40.00%) | Memory: 30 MB (66.25%) - LeetSync

---
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        st = []
+        ans = 0
+
+        for num in nums:
+            while len(st) > 0 and num < st[-1]:
+                st.pop()
+
+            if len(st) == 0 or num > st[-1]:
+                if num == 0:
+                    continue
+                ans += 1
+                st.append(num)
+
+        return ans
