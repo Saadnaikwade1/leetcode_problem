5a2b6fa12cb459ca67250b4aed59942d2042ee79
Saad Naikwade <134310173+Saadnaikwade1@users.noreply.github.com>
2026-01-02 10:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (41.46%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -1,9 +1,8 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        n=len(nums)
-        freq={}
-        for i in range(n):
-            freq[nums[i]]=freq.get(nums[i],0)+1
-        for k,v in freq.items():
-            if v==(n//2):
-                return k
+        # optimal
+        seen=set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
